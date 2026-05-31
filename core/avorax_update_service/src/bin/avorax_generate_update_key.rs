f4cae7f7b9e4fded4c7f97e7f3a85ccdded62ee7
PATERIK.BE <paterikjanfederik@gmail.com>
2026-05-31 21:49:41 +0200

    tools: add Avorax update key generator

---
+++ core/avorax_update_service/src/bin/avorax_generate_update_key.rs
@@ -0,0 +1,12 @@
+use anyhow::Result;
+use ed25519_dalek::SigningKey;
+
+fn main() -> Result<()> {
+    let mut seed = [0_u8; 32];
+    getrandom::getrandom(&mut seed)?;
+    let signing_key = SigningKey::from_bytes(&seed);
+    let verifying_key = signing_key.verifying_key();
+    println!("private={}", hex::encode(seed));
+    println!("public={}", hex::encode(verifying_key.to_bytes()));
+    Ok(())
+}
